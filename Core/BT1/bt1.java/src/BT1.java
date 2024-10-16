06f5f8cd8ac763d34733bebd44f6d01214b4e005
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2024-10-16 20:32:30 +0700

    Add files via upload

---
+++ Core/BT1/bt1.java/src/BT1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BT1 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Nhap n = ");
+        int n = sc.nextInt();
+        
+
+        if(n >= 0){
+            System.out.println("Day la so nguyen duong");
+
+        }else{
+        System.out.println("Day la so nguyen am");
+
+        }
+    }
+}
