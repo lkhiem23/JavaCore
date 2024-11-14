923400e70c179e66ff6f5287b2f1ab8eeece0979
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2024-11-14 22:55:23 +0700

    Update Main.java

--- Core/PhuongTrinh_So/src/Main.java
+++ Core/PhuongTrinh_So/src/Main.java
@@ -1,4 +1,3 @@
-// Replace ___ with you code
 import java.util.Scanner;
 
 class Main {
@@ -7,10 +6,7 @@ class Main {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
 
-        // iterate the outer loop n times
         for (int i = 0; i <= n; ++i) {
-
-            // iterate the inner loop 2 times
             for (int j = 0; j < i; ++j) {
                 System.out.println("*");
             }
