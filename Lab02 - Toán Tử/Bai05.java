f4f5368173a34b6934a7d1c921c3758ef4fb61b0
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2024-11-19 23:51:51 +0700

    Update Bai05.java

--- Lab02 - Toán Tử/Bai05.java
+++ Lab02 - Toán Tử/Bai05.java
@@ -6,14 +6,12 @@ public class Bai05 {
     public static void main(String[] args) {
         int a;
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Kiểm tra số chẵn lẻ");
 
 
         do{
             System.out.println("\nNhập vào số nguyên N: ");
             a = sc.nextInt();
-
             if (a%2 == 0){
                 System.out.println("Đây là số chẵn.");
             } else {
