243528aef785ce3a6c82c5841d8878b65be24e91
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2024-12-01 19:47:48 +0700

    Update Lab01_OOP.java

--- Lab_OnTap/Lab01_OOP.java
+++ Lab_OnTap/Lab01_OOP.java
@@ -1,7 +1,7 @@
 public class Lab01_OOP {
     public static void main(String[] args) {
         Employee[] staff = new Employee[3];
-        staff[0] = new Employee("Khiem", 30000);
+        staff[0] = new Employee("Khiem", 35000);
         staff[1] = new Employee("Hoai", 40000);
         staff[2] = new Employee("Le", 20000);
 
