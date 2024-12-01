43d8d7e73d3084a553697818e89933bcdffe3c0e
Lê Hoài Khiêm <101650006+lkhiem23@users.noreply.github.com>
2024-12-01 19:48:15 +0700

    Update Bai3.java

--- OOP/src/Bai3/Bai3.java
+++ OOP/src/Bai3/Bai3.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package Bai3;
 
-/**
- *
- * @author ADMIN
- */
 public class Bai3 {
      private String name;
         private String position;
